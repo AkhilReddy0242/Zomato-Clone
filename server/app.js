4d592f63ecdaecf09253b30176148f1933b55312
Akhil Reddy <53377293+AkhilReddy0242@users.noreply.github.com>
2022-12-02 03:54:52 +0530

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const express = require("express");
+const bodyparser = require('body-parser');
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+
+//components
+const apiRouter = require("./app/router/api-router");
+
+  const DBStiring ="mongodb://localhost:27017/Zomato-Final";
+
+  const PORT = process.env.PORT || 8080;
+  
+  mongoose.connect(DBStiring,()=>{console.log("database connected successfully")},e=>console.log("Error 404 in database server",e));
+  const app = express();
+
+  app.use(cors());
+  app.use(bodyparser.json());
+  if(process.env.NODE_ENV == "production"){
+    app.use(express.static("client/build"));
+    const path =require('path');
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,"client","build","index.html"))
+    })
+  }
+app.use("/api", apiRouter);
+app.listen(PORT,()=>console.log(`server is running on the port : ${PORT}`));
+
+
