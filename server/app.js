6c8feb53656e9d2b7ca8ab517601a8cb69d607d3
Akhil Reddy <53377293+AkhilReddy0242@users.noreply.github.com>
2022-12-02 15:18:17 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,7 +5,6 @@ const bodyparser = require('body-parser');
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-
 //components
 const apiRouter = require("./app/router/api-router");
 
@@ -21,7 +20,7 @@ const apiRouter = require("./app/router/api-router");
   if(process.env.NODE_ENV == "production"){
     app.use(express.static("client/build"));
     const path =require('path');
-    app.get('*',(req,res)=>{
+    app.get('./',(req,res)=>{
         res.sendFile(path.resolve(__dirname,"client","build","index.html"))
     })
   }
